22049c0a3b0f12ec0f179b6f2d89fda4b236888d
KrolLew23 <aniabandola@gmail.com>
2024-01-19 05:10:12 +0100

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const itemsList = document.createElement('li');
+  itemsList.textContent = ingredient;
+  itemsList.classList.add('item');
+  ingredientsList.appendChild(itemsList);
+});
