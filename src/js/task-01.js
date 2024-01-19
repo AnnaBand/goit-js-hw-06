cd34db7df76ac3dfa2b57eac24ef2861fea601eb
KrolLew23 <aniabandola@gmail.com>
2024-01-19 02:03:05 +0100

    task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+const categoriesList = document.querySelector('#categories');
+
+const itemsList = categoriesList.querySelectorAll('li.item');
+
+const numberOfCategories = itemsList.length;
+
+console.log(numberOfCategories);
+
+itemsList.forEach(item => {
+    const headerTwo = item.querySelector('h2');
+    const elements = item.querySelectorAll('li');
+    console.log(`Category: ${headerTwo.textContent}`);
+    console.log(`Elements: ${elements.length}`);
+});
