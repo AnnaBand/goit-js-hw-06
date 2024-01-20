46e1e4f3963fbbc29722f09906005d79ba5ca2b4
KrolLew23 <aniabandola@gmail.com>
2024-01-20 02:07:36 +0100

    task-05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', () => {
-  const userName = nameInput.value;
+  const userName = nameInput.value.trim();
 
   if (userName === '') {
     nameOutput.textContent = 'Anonymous';
