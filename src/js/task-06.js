64831ea7d9133672a5e647e469ae5e1242021749
KrolLew23 <aniabandola@gmail.com>
2024-01-20 01:38:42 +0100

    task-06

---
+++ src/js/task-06.js
@@ -0,0 +1,15 @@
+const validationInput = document.querySelector('#validation-input');
+
+validationInput.addEventListener('blur', () => {
+    const requiredValueLength = parseInt(validationInput.getAttribute('data-length'));
+    const inputValueLength = validationInput.value.length;
+
+    validationInput.classList.remove('valid', 'invalid');
+
+    if (requiredValueLength === inputValueLength) {
+        validationInput.classList.add('valid');
+    }
+    else {
+        validationInput.classList.add('invalid');
+    }
+});
