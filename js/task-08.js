9b9184c31d5c66a62241615529ba78e854242153
KrolLew23 <aniabandola@gmail.com>
2024-01-21 15:30:35 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+document.querySelector('.login-form').addEventListener('submit', event => {
+    event.preventDefault();
 
+    const email = event.currentTarget .elements.email.value;
+    const password = event.currentTarget .elements.password.value;
+
+    if (email === '' || password === '') {
+        alert('This field is required.');
+        return;
+    }
+
+    const formData = {
+        email,
+        password
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+});
